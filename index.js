467b52ab3eb014d228bd8a6a83d1d1a82ea27f63
Lucas <cekanlucas@gmail.com>
2020-02-14 22:22:22 +0000

    final commit

--- index.js
+++ index.js
@@ -1,44 +1,45 @@
-const taskObj = {
-  task : {
+/*   task # : { //task template
     title        :'title',
     description  :'description',
     complete     : false
-  }
-};
-console.log(taskObj)
+  } */
 
-// unique task id generator
-const newID = () => {
+const taskObj = {};
+
+const newID = () => { // unique task id generator
   let i = 0;
-  while( taskobj[`task${i}`]){
+  while (taskObj[`task ${i}`]) {
     i++;
   }
-  return `task${i}`;
-}
+  return `task ${i}`;
+};
 
 // add task to taskObj
-const newTask = (task, title, description) => {
-  taskObj[task] = {
-    title,
-    description,
+const newTask = (title, description) => {
+  const newtask = newID();
+  taskObj[newtask] = {
+    title : title,
+    description : description ? description : null,
     complete : false
-  }
-  console.log(`A new task has been created ${title}`)
-}
+  };
+  console.log(`A new task has been created ${title}`);
+};
 
 // Mark a task as complete by setting the task's status in the taskObj to `true`
-function completeTask(task) {
-  const completedTask = taskObj[task].title
-  taskObj[task] = true;
-  console.log(`The task ${completeTask} has been completed`)
-}
+const completeTask = taskNumber => {
+  const task = `task ${taskNumber}`;
+  const completedTask = taskObj[task].title;
+  taskObj[task].complete = true;
+  console.log(`The task ${completedTask} has been completed`);
+};
 
 // Print the state of a task to the console in a nice readable way
-function logTaskState(task) {
+const logTaskState = taskNumber => {
+  const task = `task ${taskNumber}`;
   const title = taskObj[task].title;
-  const complete = taskObj[task].complete;
-  console.log(`${title} has${complete ? " " : " not "}been completed`);
-}
+  const completeVar = taskObj[task].complete;
+  console.log(`${title} has${completeVar ? " " : " not "}been completed`);
+};
 
 // DRIVER CODE BELOW
 
@@ -48,7 +49,3 @@ newTask("Do Laundry"); // task 1
 logTaskState(0); // Clean Cat Litter has not been completed
 completeTask(0);
 logTaskState(0); // Clean Cat Litter has been completed
-
-
-// Task Object
-
